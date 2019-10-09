1f9b827cebc827ced745d371460a8a1a06699f20
TheR3Dplay <39969659+TheR3Dplay@users.noreply.github.com>
2019-10-09 19:01:45 +0200

    Teleport

---
+++ Laberint_Gerard_Redondo_01/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D rb;
+
+    private Vector2 movement;
+    public float speed = 50.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition((Vector2)transform.position + (movement * speed * Time.deltaTime));
+    }
+}
---
+++ Laberint_Gerard_Redondo_01/Assets/Scripts/PortalOriginController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalOriginController : MonoBehaviour
+{
+    public GameObject destination;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D collider)
+    {
+        Debug.Log("Collision detected with " + collider.gameObject.ToString());
+        collider.gameObject.transform.position = destination.transform.position;
+    }
+}
