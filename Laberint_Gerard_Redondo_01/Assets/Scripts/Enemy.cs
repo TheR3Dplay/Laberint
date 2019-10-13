8395fe0d8cdd1c57544621955a2d73bac8956808
TheR3Dplay <39969659+TheR3Dplay@users.noreply.github.com>
2019-10-14 01:53:42 +0200

    Enemic

---
+++ Laberint_Gerard_Redondo_01/Assets/Scripts/Enemy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    public float speed;
+    public float lenght;
+    private float counter;
+    private float startPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = transform.position.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        counter += Time.deltaTime * speed;
+        transform.position = new Vector2(Mathf.PingPong(counter, lenght) + startPosition, transform.position.y);
+    }
+
+    private void OnTriggerEnter2D(Collision2D jugador)
+    {
+        jugador.gameObject.GetComponent<PlayerController>().vida -= 30;
+    }
+}
